f989d64b1b0c6896b10559691acbd431fdec7ca0
Andrea Locatelli <ciao.loca@gmail.com>
2024-10-04 12:44:41 +0200

    feat: prettier and new font

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from 'astro:content'
 
 const blog = defineCollection({
 	type: 'content',
@@ -9,8 +9,8 @@ const blog = defineCollection({
 		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
-});
+		heroImage: z.string().optional()
+	})
+})
 
-export const collections = { blog };
+export const collections = { blog }
